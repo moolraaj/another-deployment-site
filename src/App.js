0281b0599db7b893af499fc63e3c26c158cf18d9
moolraaj <116911577+moolraaj@users.noreply.github.com>
2022-11-21 03:34:27 -0800

    changes done
    changes done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <p>this is a paragraph</p>
     <a href="https://www.youtube.com/watch?v=Cdg2FGDMglk&list=PLu0W_9lII9agwhy658ZPA0MTStKUJTWPi&index=3">click here & go to next page</a>
     <p>this is a paragraph</p>
+    <h1>this is a heading on app js to showing front of the page</h1>
     </>
   );
 }
