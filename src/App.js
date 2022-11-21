259645b1a46b40fbd35ebf44985abf7e1ffe9621
moolraaj <116911577+moolraaj@users.noreply.github.com>
2022-11-21 02:36:50 -0800

    my-first-commit-on-this

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <p>this is a paragraph</p>
+    <a href="https://www.youtube.com/watch?v=Cdg2FGDMglk&list=PLu0W_9lII9agwhy658ZPA0MTStKUJTWPi&index=3">click here & go to next page</a>
+    </>
   );
 }
 
