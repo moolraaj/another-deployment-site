37a4a7d69df2684dd71d67deddd75d1ef3beaec4
moolraaj <116911577+moolraaj@users.noreply.github.com>
2022-11-21 04:59:37 -0800

    changes into main branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
     <a href="https://www.youtube.com/watch?v=Cdg2FGDMglk&list=PLu0W_9lII9agwhy658ZPA0MTStKUJTWPi&index=3">click here & go to next page</a>
     <p>this is a paragraph</p>
     <h1>this is a heading on app js to showing front of the page</h1>
+    
     <h1>loream ipsum in app js</h1>
+    <h1>loream ipsum in app js</h1>
+
     </>
   );
 }
